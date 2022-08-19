fc6061ac2983086e8cac81bacfa735c63b32a4a1
Luke Bemish <lukebemish@gmail.com>
2022-08-18 20:14:26 -0600

    Update brainmap lexer again

--- lib/rouge/lexers/brainmap.rb
+++ lib/rouge/lexers/brainmap.rb
@@ -22,9 +22,8 @@ module Rouge
 
         types = %w(boolean byte char double float int long short var void)
 
-        id = /[[:alpha:]_][[:word:]]*/
-        const_name = /[[:upper:]][[:upper:][:digit:]_]*\b/
-        class_name = /[[:upper:]][[:alnum:]]*\b/
+        class_name = /[a-zA-Z][a-zA-Z0-9$\.]*/
+        id = /[a-zA-Z][a-zA-Z0-9$]*/
 
         state :root do
           rule %r/[^\S\n]+/, Text
@@ -32,38 +31,36 @@ module Rouge
           rule %r(/\*.*?\*/)m, Comment::Multiline
           # keywords: go before method names to avoid lexing "throw new XYZ"
           # as a method signature
-          rule %r/(?:#{keywords.join('|')})\b/, Keyword
-
-          rule %r(
-            (\s*(?:[a-zA-Z_][a-zA-Z0-9_.\[\]<>]*\s+)+?) # return arguments
-            ([a-zA-Z_][a-zA-Z0-9_]*)                  # method name
-            (\s*)(\()                                 # signature start
-          )mx do |m|
-            # TODO: do this better, this shouldn't need a delegation
-            delegate Brainmap, m[1]
-            token Name::Function, m[2]
-            token Text, m[3]
-            token Operator, m[4]
-          end
-
-          rule %r/@\??#{id}/, Name::Decorator
+          rule %r/(#{keywords.join('|')})/, Keyword
+          rule %r/@\??#{class_name}/, Name::Decorator
           rule %r/(?:#{declarations.join('|')})\b/, Keyword::Declaration
-          rule %r/(?:#{types.join('|')})\b/, Keyword::Type
-          rule %r/(?:true|false)\b/, Keyword::Constant
+
+          rule %r/(#{types.join('|')})/, Keyword::Type
+          rule %r/(true|false)/, Keyword::Constant
           rule %r/"(\\\\|\\"|[^"])*"/, Str
           rule %r/'(?:\\.|[^\\]|\\u[0-9a-f]{4})'/, Str::Char
-          rule %r/(\.)(#{id})/ do
-            groups Operator, Name::Attribute
+
+          rule %r(#{id}(\.#{id})+), Name::Attribute
+
+          rule %r(
+              (#{id})
+              (\s*)(\()
+          )mx do |m|
+            token Name::Function, m[1]
+            token Text, m[2]
+            token Operator, m[3]
           end
 
-          rule const_name, Name::Constant
-          rule class_name, Name::Class
-          rule %r/\$?#{id}/, Name
-          rule %r/[(){},=]|(->)/, Operator
+          rule %r(#{id}), Name::Constant
+
+          rule class_name, Name::Attribute
+          rule %r/\$?#{id}/, Name::Attribute
+          rule %r/[(){},=#]|(->)/, Operator
 
           digit = /[0-9]_+[0-9]|[0-9]/
           rule %r/#{digit}+\.#{digit}+[fd]?/, Num::Float
           rule %r/#{digit}+[lLsSbBiI]?/, Num::Integer
+          rule %r/\s/, Text
           rule %r/\n/, Text
         end
       end
